2f3c932d80c20e6ee5ac0e4eae1ff1f7387edaa5
alexabravo <42972619+alexabravo@users.noreply.github.com>
2021-09-26 00:36:55 -0600

    Parte 1 y 2 listaaa!

--- lab7.py
+++ lab7.py
@@ -1,3 +1,40 @@
+#------------MCD EUCLIDES----------
+def mcdEuclides(n1, n2):
+    
+    while n1 % n2 != 0:
+        sobrante = n1 % n2
+        n1 = n2
+        n2 = sobrante
+        
+    return n2
+
+print("MCD utilizando el algoritmo de Euclides") 
+print("Para 1036 y 240 el resultado es:", mcdEuclides(1036, 240))
+print("Para 22896 y 192 el resultado es:", mcdEuclides(22896, 192))
+print("Para 689161 y 378851 el resultado es:", mcdEuclides(689161, 378851))
+
+
+#------------MCD EUCLIDES EXTENDIDO----------
+def euclidesExtendido(n1, n2):
+    x,y, u,v = 0,1, 1,0
+    while n1 != 0:
+        q, r = n2//n1, n2%n1
+        m, n = x-u*q, y-v*q
+        n2,n1, x,y, u,v = n1,r, u,v, m,n
+    gcd = n2
+    return gcd, x, y
+
+print("-------------------------------------------------------------------")
+print("Algoritmo de Euclides Extendido")
+print("El primer valor corresponde al mcd, seguido de los valores de x y y")
+print("Para 1036 y 240 el resultado es:" ,euclidesExtendido(1036,240))
+print("Para 8753 y 3354 el resultado es:" ,euclidesExtendido(8753,3354))
+print("Para 2021 y 43 el resultado es:" ,euclidesExtendido(2021,43))
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -29,7 +66,8 @@ def generador(longitud):
 
     return(int(numero))
 
-
+print("-------------------------------------------------------------------")
+print("Fermat Test")
 fermatTest(11, 5)
 
 
@@ -38,5 +76,3 @@ while (numerosGenerados<5):
     if(resultado[0] == True):
         print(resultado[1])
         numerosGenerados+=1
-        
-
